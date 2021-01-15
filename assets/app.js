903251d841a982ef59a96d923ffbe4bf5a1fbdc2
Anu Gulati <anu.gulati100@gmail.com>
2021-01-15 17:56:29 -0500

    webpack encore wooo

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,22 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+import $ from 'jquery';
+
 // start the Stimulus application
 import './bootstrap';
+/**
+ * Simple (ugly) code to handle the comment vote up/down
+ */
+var $container = $('.js-vote-arrows');
+$container.find('a').on('click', function(e) {
+    e.preventDefault();
+    var $link = $(e.currentTarget);
+    $.ajax({
+        url: '/comments/10/vote/'+$link.data('direction'),
+        method: 'POST'
+    }).then(function(data) {
+        $container.find('.js-vote-total').text(data.votes);
+    });
+});
